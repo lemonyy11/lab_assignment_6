f36b1a36119a842094499d5512343a8c0d01b8c4
lemonyy11 <125504096+lemonyy11@users.noreply.github.com>
2023-02-24 08:05:11 -0500

    Update lab_assignment_6.c

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -6,10 +6,10 @@ int search(int numbers[], int low, int high, int value)
     if(low <= high){
         mid = (low + high)/2;
         if(value < numbers[mid]){
-            return binSearch(numbers, low, mid - 1, value);
+            return search(numbers, low, mid - 1, value);
         }
         else if(value > numbers[mid]){
-            return binSearch(numbers, low, mid + 1, value);
+            return search(numbers, low, mid + 1, value);
         }
         else{
             return -1;
